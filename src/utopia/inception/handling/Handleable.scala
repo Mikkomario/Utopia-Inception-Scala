c51070aa173fc0e58ed5b25806e3f0d988d8751e
Mikkomario <mikko_hilpinen@hotmail.com>
2016-10-19 21:27:05 +0300

    Added some features to handleable

--- src/utopia/inception/handling/Handleable.scala
+++ src/utopia/inception/handling/Handleable.scala
@@ -13,6 +13,12 @@ trait Handleable extends Killable
     private val handlingStates = new HashMap[HandlerType, Boolean]()
     
     /**
+     * The object this handleable depends from. The object's handling state must be true in order 
+     * for this object to be handled.
+     */
+    var dependsFrom: Option[Handleable] = None
+    
+    /**
      * This is the state used when a handling state hasn't been specified for a specific handler
      */
     var defaultHandlingState = false
@@ -20,11 +26,16 @@ trait Handleable extends Killable
     /**
      * The handling state (should the object be interacted) for a certain handler type
      */
-    def handlingState(handlerType: HandlerType) = 
+    def handlingState(handlerType: HandlerType): Boolean = 
     {
-        val specifiedState = handlingStates.get(handlerType)
-        
-        specifiedState.getOrElse(defaultHandlingState)
+        // If dependent object is specified and its state is false, can't have true state
+        if (dependsFrom.isDefined && !dependsFrom.get.handlingState(handlerType))
+            return false;
+        else
+        {
+            val specifiedState = handlingStates.get(handlerType)
+            specifiedState.getOrElse(defaultHandlingState)
+        }
     }
     
     /**
@@ -34,5 +45,14 @@ trait Handleable extends Killable
      * @return Whether there was a specific state defined previously
      */
     def specifyHandlingState(handlerType: HandlerType, state: Boolean) = 
-        handlingStates.put(handlerType, state).isDefined
+        handlingStates.put(handlerType, state).isDefined;
+    
+    /**
+     * Specifies the handling state for a specific handler type so that the default state is 
+     * no longer used. The handling state stays the same until it is changed, however.
+     * @param handlerType the handlerType for which the state is specified
+     * @return whether an existing specification was overwritten
+     */
+    def specifyHandlingState(handlerType: HandlerType): Boolean = 
+        specifyHandlingState(handlerType, handlingState(handlerType));
 }
